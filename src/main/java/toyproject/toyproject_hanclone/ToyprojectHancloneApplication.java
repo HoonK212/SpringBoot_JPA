f2024345458811000abc7e52c07214ac3e86bc60
Hoon Kim <b0aeiou@gmail.com>
2021-01-07 18:58:25 +0900

    내용 : spring boot starter 생성

---
+++ src/main/java/toyproject/toyproject_hanclone/ToyprojectHancloneApplication.java
@@ -0,0 +1,13 @@
+package toyproject.toyproject_hanclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectHancloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyprojectHancloneApplication.class, args);
+    }
+
+}
